5a1028c447799a27dee28025007122d06c5c92ed
kokoetebassey <110944955+kokoetebassey@users.noreply.github.com>
2023-04-05 00:48:27 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+// import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+
+
+
+
+
+ function App() {
+  return (
+
+     <div className="Mainscreen">
+      <Navbar />
+        {/* <Routes> */}
+          {/* <Route path="AdminaDashboard" element={<AdminaDashboard />}></Route> */}
+        {/* </Routes> */}
+      </div>
+   
+
+  
+  );
+}
+
+export default App;
